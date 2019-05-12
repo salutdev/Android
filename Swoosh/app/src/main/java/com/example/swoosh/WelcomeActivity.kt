ae383b3474cf7202986247e189247e4064f7f9d8
salutdev <salutsirius@gmail.com>
2019-05-12 16:28:50 -0700

    Add activities transition

--- Swoosh/app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ Swoosh/app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -1,12 +1,19 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.welcome_activity.*
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_activity)
+
+        getStartedBtn.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
