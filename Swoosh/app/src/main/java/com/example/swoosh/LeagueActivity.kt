2cd16e7ba6891923a829471f3d37fc37401a2a2c
salutdev <salutsirius@gmail.com>
2019-05-13 17:07:03 -0700

    Create skill screen

--- Swoosh/app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ Swoosh/app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -1,12 +1,24 @@
 package com.example.swoosh
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+//        leagueNextBtn.setOnClickListener {
+//
+//            val skillIntent = Intent(this, SkillActivity::class.java)
+//            startActivity(skillIntent)
+//        }
+    }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
     }
 }
