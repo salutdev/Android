1f7d486e01fda50567370ac8bf004988b017cdb3
salutdev <salutsirius@gmail.com>
2019-05-20 18:26:28 -0700

    Implement product screen with grid view and recycler adapter

---
+++ CoderSwag/app/src/main/java/com/example/coderswag/Adapters/ProductAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.coderswag.Adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.coderswag.Model.Product
+import com.example.coderswag.R
+
+class ProductAdapter(private val context: Context, private val products: List<Product>, private val itemClick: (Product) -> Unit) : RecyclerView.Adapter<ProductAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_list_item, parent, false)
+        return Holder(view, itemClick)
+    }
+
+    override fun getItemCount(): Int {
+        return products.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bindProduct(products[position], context)
+    }
+
+    inner class Holder(itemView: View, val itemClick: (Product) -> Unit) : RecyclerView.ViewHolder(itemView) {
+
+        val productImage = itemView.findViewById<ImageView>(R.id.productImage)
+        val productName = itemView.findViewById<TextView>(R.id.productName)
+        val productPrice = itemView.findViewById<TextView>(R.id.productPrice)
+
+        fun bindProduct(product: Product, context: Context) {
+            val resourceId = context.resources.getIdentifier(product.image, "drawable", context.packageName)
+            productImage?.setImageResource(resourceId)
+            productName?.text = product.title
+            productPrice?.text = product.price
+            itemView.setOnClickListener { itemClick(product) }
+        }
+    }
+}
--- CoderSwag/app/src/main/java/com/example/coderswag/Controller/ProductActivity.kt
+++ CoderSwag/app/src/main/java/com/example/coderswag/Controller/ProductActivity.kt
@@ -1,18 +1,47 @@
 package com.example.coderswag.Controller
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
+import com.example.coderswag.Adapters.ProductAdapter
 import com.example.coderswag.R
+import com.example.coderswag.Services.DataService
 import com.example.coderswag.Utitities.EXTRA_CATEGERY
+import kotlinx.android.synthetic.main.activity_product.*
 
 class ProductActivity : AppCompatActivity() {
+
+    lateinit var productAdapter: ProductAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
         val categoryTitle = intent.getStringExtra(EXTRA_CATEGERY)
-        Toast.makeText(this, "$categoryTitle is clicked", Toast.LENGTH_SHORT).show()
+        val listOfProducts = DataService.getProducts(categoryTitle)
+
+        productAdapter = ProductAdapter(this, listOfProducts) { product ->
+            Toast.makeText(this, "${product.title} is clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        productListView.adapter = productAdapter
+
+        var colNum = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            colNum = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            colNum = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, colNum)
+        productListView.layoutManager = layoutManager
+        productListView.hasFixedSize()
     }
 }
