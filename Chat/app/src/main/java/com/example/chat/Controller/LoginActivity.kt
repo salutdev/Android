6724922a20e5a2c423541c4fa388a0a7c3a99a7d
salutdev <salutsirius@gmail.com>
2019-06-03 17:19:44 -0700

    Finish login activity

--- Chat/app/src/main/java/com/example/chat/Controller/LoginActivity.kt
+++ Chat/app/src/main/java/com/example/chat/Controller/LoginActivity.kt
@@ -20,5 +20,6 @@ class LoginActivity : AppCompatActivity() {
     fun loginCreateUserBtnClicked(view: View) {
         val intent = Intent(this, CreateUserActivity::class.java)
         startActivity(intent)
+        finish()
     }
 }
