df5838dda6820fb1979ca74abc822d1c9c0b469f
salutdev <salutsirius@gmail.com>
2019-09-19 14:01:26 -0700

    Display photo on details view

--- FlickrBrowser/app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
+++ FlickrBrowser/app/src/main/java/com/example/flickrbrowser/PhotoDetailsActivity.kt
@@ -2,21 +2,37 @@ package com.example.flickrbrowser
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
 
 import kotlinx.android.synthetic.main.activity_photo_details.*
+import kotlinx.android.synthetic.main.browse.view.*
+import kotlinx.android.synthetic.main.content_photo_details.*
 
-class PhotoDetailsActivity : AppCompatActivity() {
+class PhotoDetailsActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_details)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        setSupportActionBar(toolbar)
+//
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+
+        activateToolbar(true)
+
+        val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_author.text = photo.author
+        photo_tags.text = photo.tags
+
+        Picasso.get().load(photo.link)
+            .error(R.drawable.placeholder)
+            .placeholder(R.drawable.placeholder)
+            .into(photo_image)
     }
 
 }
--- FlickrBrowser/app/src/main/java/com/example/flickrbrowser/SearchActivity.kt
+++ FlickrBrowser/app/src/main/java/com/example/flickrbrowser/SearchActivity.kt
@@ -1,19 +1,23 @@
 package com.example.flickrbrowser
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_search.*
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : BaseActivity() {
+
+    private val TAG = "SearchActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate starts")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        setSupportActionBar(toolbar)
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        setSupportActionBar(toolbar)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        activateToolbar(true)
+        Log.d(TAG, "onCreate ends")
     }
 
 }
